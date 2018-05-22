059b2f84a2e701379edfea6ee692451f3f96c85a
Ke YU <kyu2@caltech.edu>
2018-05-22 16:12:38 -0700

    minor

--- Kevin_Helmholtz.jl
+++ Kevin_Helmholtz.jl
@@ -1,29 +1,34 @@
 push!(LOAD_PATH, pwd())
+dir = "/home/kyu/Turbulence_Box"
+push!(LOAD_PATH, dir)
 FFTW.set_num_threads(Sys.CPU_CORES)
-#FFTW.set_num_threads(1)
 import input;reload("input")
 import solver;reload("solver")
+import TB_types
 import sol_show;
 using JLD
 
-N = 196
-Re = 200
-max_tstep_cnt = 1800
-output_freq = 30
+N = 256
+Re = 300
+max_tstep_cnt = 1
+output_freq = 60
 ICC = "KevinHelmholtz"
 #ICC = "ABC"
 RK = 2
-#ICC = "restart"
 
+ICC = "restart"
 
 input_ = input.input_init(N, Re, ICC, max_tstep_cnt, output_freq, RK)
 
 if ICC == "restart"
+    fname = "KH_N_256_Re_300.jld"
+    s = load(dir*"/data/" * fname)
+    sols = s["sols"]
     solver_ = solver.init_solver(input_, sols);
 else
     solver_ = solver.init_solver(input_);
 end
 sols = solver_.solve(solver_);
 
-fname = "./data/KH_N_" * string(N) *"_Re_" * string(Re) * ".jld"
+fname = dir*"/data/KH_N_" * string(N) *"_Re_" * string(Re) * "_long.jld"
 save(fname, "sols", sols)
--- output_script.jl
+++ output_script.jl
@@ -1,10 +1,10 @@
 push!(LOAD_PATH, pwd())
 using JLD
 import TB_output; reload("TB_output")
+dir = "/home/kyu/Turbulence_Box"
 fname = "KH_N_256_Re_300.jld"
 
-s = load("data/" * fname)
+s = load(dir * "/data/" * fname)
 sols = s["sols"]
 
-#vtk= TB_output.write_vtk_single(sols.sol[21], fname)
 pvd = TB_output.write_vtk_series(sols, "pvd_KH_128")
